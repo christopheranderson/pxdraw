7c09e1267fd6504f08468a8669f5799528586a28
Christopher Anderson <chrande@microsoft.com>
2018-04-18 14:12:29 -0700

    add app insights (and tweak the webpack.config.json) (#14)
    * add app insights (and tweak the webpack.config.json)
    
    * add batching to updatepixel (#13)
    
    * Update canvas periodically (every 30s) (#15)
    
    * Canvas auto-size. Fixed zoom, pan for pixel draw. New layout. Smaller palette.
    
    * Fix palette area
    
    * Remove scroll panning buttons
    
    * Move twitter and pixel coordinates into canvas
    
    * More space between palette buttons. Styling of timer and login
    
    * Cosmetic header css changes
    
    * Coordinates display now draggable. Bind ko to Main instead of Canvas
    
    * Remove test code in updateClient
    
    * Update header with proper color cosmosdb logo
    
    * Fix fetch board code (untested)
    
    * Now fetch board at 30s interval
    
    * Logs for update replays
    
    * add app insights (and tweak the webpack.config.json)

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './built/main.js',
@@ -7,5 +8,15 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'production',
-    devtool: 'eval-source-map'
+    devtool: 'eval-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            inject: false,
+            filename: "../index.html",
+            templateParameters:{
+                appInsightsKey: process.env.PXDRAW_APPINSIGHTS_KEY || "<instrumentation key here>",
+            }
+        })
+    ]
 };
