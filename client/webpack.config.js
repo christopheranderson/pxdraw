ad668dc4b3407a20d4886dd672726ae74ed3806f
Laurent Nguyen <laurent.nguyen@microsoft.com>
2018-04-18 09:33:17 +0200

    Canvas auto-size. Fixed zoom, pan for pixel draw. New layout. Smaller palette.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './built/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './built/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production',
+    devtool: 'eval-source-map'
 };
