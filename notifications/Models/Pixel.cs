937a941d3dadbc04acc87973f806d81f563e1140
Matias Quaranta <ealsur@users.noreply.github.com>
2018-05-04 14:41:09 -0300

    Adding more operational logging (#37)
    * Adding more operational logging
    
    * Adding pagesize event
    
    * Removing Flush to improve working latency
    
    * Cleaning up

--- notifications/Models/Pixel.cs
+++ notifications/Models/Pixel.cs
@@ -4,15 +4,16 @@
 
 namespace PxDRAW.SignalR.Models
 {
-    using System;
     using Newtonsoft.Json;
 
     internal class Pixel
     {
         [JsonProperty("x", Required = Required.Always)]
         public int X { get; set; }
+
         [JsonProperty("y", Required = Required.Always)]
         public int Y { get; set; }
+
         [JsonProperty("color", Required = Required.Always)]
         public int Color { get; set; }
     }
